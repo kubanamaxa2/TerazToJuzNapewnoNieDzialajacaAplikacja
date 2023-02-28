f8dc6a12bea54f4df4ce8f116b7e006bbb929c0a
Dominos0321 <dsch792@gmail.com>
2023-02-28 09:10:01 +0100

    Coś tam mam

--- app/src/main/java/com/example/teraztojuznapewnoniedzialajacaaplikacja/MainActivity.kt
+++ app/src/main/java/com/example/teraztojuznapewnoniedzialajacaaplikacja/MainActivity.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-     findViewById<Button>(R.id.button).setOnClickListener{
+
+        findViewById<TextView>(R.id.txtvw1).setOnClickListener{
+            Toast.makeText(applicationContext, "Login = Imię | Hasło = Nazwisko", Toast.LENGTH_LONG).show()
+        }
+     findViewById<Button>(R.id.btn1).setOnClickListener{
+
          val intent = Intent(this, DaneUz::class.java)
          startActivity(intent)
      }
